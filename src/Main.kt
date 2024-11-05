2f0022061bae8eda1da801b7f9b8ab356015e702
anabelgv <a23anabelgv@iessanclemente.net>
2024-11-05 10:28:59 +0100

    actualizado

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,63 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val intentosmax = 8
+    val numeroSecreto = generateNumeroSecreto()
+    val intentos = mutableListOf<List<String>>()
+    val contadorintentos = 0
+    var finjuego = false
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+
+    while (!finjuego && contadorintentos < intentosmax) {
+        println("1.Jugar")
+        println("2.Ver traza de último intento")
+        println("3.Salir")
+        print("opción: ")
+        val option = readln()
+
+        when (option) {
+            "1" -> {
+                contadorintentos++
+                println("Teclea un numero de 3 cifras usando solo numero del 1 al 6 sin ser repetidos.")
+                val adivina = readln()
+
+                if(adivina!= null && numerovalido(adivina)){
+                    val(acierto,error) = validacion(adivina,numeroSecreto)
+                    intentos.add(listOf(adivina,acierto.toString(),error.toString()))
+                    if (acierto == 3){
+                        println("Felicidades, adivinaste el numero secreto $numeroSecreto en $contadorintentos  intento/s!")
+
+                        finjuego = true
+                    }else{
+                        println("Numero no válido.")
+                    }
+                    if (contadorintentos >= intentosmax && !finjuego){
+                        println("No adivinaste el numero $numeroSecreto en $intentosmax intentos.")
+                        finjuego=true
+                    }
+                }
+            }
+            "2"->{ //ARREGLAR
+                if (intentos.isNotEmpty()){
+                    println("Numero secreto:$numeroSecreto")
+                    for ((index,intentos) in intentos.withIndex()){}
+                }
+            }
+            "3" ->{
+                println("Gracias por jugar")
+                finjuego=true
+            }else-> println("Opcion no valida.Vuelve a intentarlo")
+        }
     }
+
+
 }
+//Arreglar
+
+fun generateNumeroSecreto(): String {
+    val digits = listOf(1, 2, 3, 4, 5, 6).shuffled().take(3)  // Obtiene 3 dígitos únicos entre 1 y 6
+    return digits.joinToString("")
+
+
+}
+
